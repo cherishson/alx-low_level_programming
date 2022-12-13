54bf7033cc549eb454062f42b22a94a25de184fc
cherishson <lovelyjaj20@gmail.com>
2022-12-13 06:21:14 -0800

    task 0 (new)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h> 
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
- *
+ * main - Entry point 
+ * Return: Always 1 (Success) 
  */
 
 int main(void) 
 {
    
-      	write;(2,  "and that piece of art is useful' - Dora Korpar, 20-10-19\n");
+      	fwrite(2, "and that piece of art is useful' - Dora Korpar, 20-10-19\n");
         return (1);
 
 } 
