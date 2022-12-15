51314e9727815ebb75f48df1bfdb6be3576cf7ee
cherishson <lovelyjaj20@gmail.com>
2022-12-15 03:52:10 -0800

    task 0 and 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print 10x the alphabet
+ *
+ * Return : Always (0)
+ *
+ */
+
+int main (void)
+{
+
+	char c; 
+
+	for (c='a'; c<='z'; c>10); c++;
+
+
+	{
+		putchar ('c');
+
+		putchar ('\n');
+	}
+
+	return 0;
+}
