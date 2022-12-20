288f056a196763b452e8d6a399a072949c548f7a
cherishson <lovelyjah20@gmail.com>
2022-12-19 17:46:04 -0800

    task 0 (new)

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,16 @@
 #include "main.h"                                                                                                                                                                                                                                                                           
 /**                                                                                                                                                    
  *
- * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98                                          *
- * on: this is the entry
+ * reset_to_98 - Set value of a variable to 98 using a pointer                                          *
+ * @n: The address of the variable needed to change
+ *
+ * Return: nothing
  *
  */                                                                                                                                                                                                                                                                                                       
 
-void reset_to_98(int *n)                                                                                                                               
-
-	                                                                                                                                                       
-	        *n = 98;                                                                                                                                    
-
+void reset_to_98(int *n)
+{	                                                                                                                                              	        *n = 98;                                                                                                                            
+}
 
 
 
