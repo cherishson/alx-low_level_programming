020c6db9343e7e0f42f85d81c2a50ad352ecf790
cherishson <lovelyjah20@gmail.com>
2022-12-22 04:10:18 -0800

    second correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void) 
 {
-    write(1, "and that piece of art is useful\" - Dora Korpar, 20-10-19\n", 56);
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2019-10-19\n", 59);
         return (1);
 
 } 
