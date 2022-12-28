ccfaffabd56d11857a438e185879aadba193dbae
cherishson <lovelyjah20@gmail.com>
2022-12-28 03:46:00 -0800

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
-#include "main.h"                                                                                                                                                                                                                                                                           
+#include "main.h"                                                                                                                                                                                                                                                                       
 /**                                                                                                                                                    
  *
- * reset_to_98 - Set value of a variable to 98 using a pointer                                          *
- * @n: The address of the variable needed to change
+ * Entry point: Take pointer to int and update value to 98 using a pointer                                          *\
  *
  * Return: nothing
  *
  */                                                                                                                                                                                                                                                                                                       
 
-void reset_to_98(int *n)
+void reset_to_98(int *n);
 {	                                                                                                                                              	        *n = 98;                                                                                                                            
 }
 
