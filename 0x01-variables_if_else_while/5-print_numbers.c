e776d40720d052f3c292a20a125587e6d8691fd0
cherishson <lovelyjah20@gmail.com>
2023-01-01 08:07:22 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Main - this is the entry point
+ *@a - the integer value to be processed
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a)i;
+	putchar ('\n');
+	return (0);
+}
