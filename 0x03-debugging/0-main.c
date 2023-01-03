058e602113a187f39485ba721dda7b2735ad0525
cherishson <lovelyjah20@gmail.com>
2023-01-03 06:32:29 -0800

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -tests functions that prints negative or positive
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
