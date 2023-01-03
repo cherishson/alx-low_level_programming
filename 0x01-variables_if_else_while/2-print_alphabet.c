ca85200bbc0dbf343087f0df31c2c6064f8bee1e
cherishson <lovelyjah20@gmail.com>
2023-01-03 00:08:43 -0800

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,22 +9,14 @@
 int main(void)
 {
 	char x;
-	char y;
 
 	x = 'a';
-	y = 'A';
 
 	while
 		(x <= 'z')
 		{putchar (x);
 		x++;
-		};
-
-	while
-		(y <= 'Z')
-		{putchar (y);
-			y++;
-		};
+		}
 
 		putchar('\n');
 	return (0);
