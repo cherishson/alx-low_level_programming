e84ea267a4c0fec36eb1bb606444475dcec762a6
cherishson <lovelyjah20@gmail.com>
2023-01-02 19:40:45 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strstr -a function that locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be located
+ *
+ * Return: if the substring is located -pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+
+					return (haystack);
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+	return ('\0');
+}
