92c45b72d58a1f8f368cfaad691465f242eda490
cherishson <lovelyjah20@gmail.com>
2023-10-09 05:40:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,28 +10,25 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '7'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = '1'; b <= '8'; b++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (c = '2'; c <= '9'; c++)
 			{
-				if (a < b && b < c)
+		
+			if ( a < b && b < c)
 			{
 				putchar(a);
 				putchar(b);
 				putchar(c);
+				putchar(',');
+				putchar(' ');
 			}
-
-		if (a != 7)
-		{
-			putchar(',');
-			putchar(' ');
+			}
 		}
-	}
-		}
-	}
-
+	}	
+	
 	putchar('\n');
 
 	return (0);
