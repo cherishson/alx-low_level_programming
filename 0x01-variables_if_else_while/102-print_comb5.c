0d72a7cc6c78d4d0c5edb0e9bd98a1fcd5e614ba
cherishson <lovelyjah20@gmail.com>
2023-10-09 07:46:23 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,15 +9,15 @@
 int main(void)
 {
 	int a, b, c, d;
+
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = '0'; d <= '9'; d++)
+				for (d = '1'; d <= 10; d++)
 				{
-
 					putchar(a);
 					putchar(b);
 					putchar(' ');
@@ -32,5 +32,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-		
-			
