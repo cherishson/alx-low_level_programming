3456f088ccd42240765b4d890057ae0f4de8e290
cherishson <lovelyjah20@gmail.com>
2023-10-10 10:02:00 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ */
+
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
