cf19d05720a4e221b1035bbde2010e06052cc69c
cherishson <lovelyjah20@gmail.com>
2023-10-11 09:47:52 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int num, times, result;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (times = 1; times <= 9; times++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = num * times;
+
+			if (result <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
