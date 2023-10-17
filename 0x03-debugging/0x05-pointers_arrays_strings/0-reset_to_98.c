df5386a1f7ef112f912b980bd35cca2f827a6540
cherishson <lovelyjah20@gmail.com>
2023-10-17 09:15:01 -0700

    0-reset_to_98.c

---
+++ 0x03-debugging/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * @n: the value it points to
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x03-debugging/0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,7 @@
+#include <ctype.h>
+
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
