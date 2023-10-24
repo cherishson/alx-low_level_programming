d35d5042de4be6d3f7a1d3d6660d154afe302fe6
cherishson <lovelyjah20@gmail.com>
2023-10-23 18:55:52 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: a pointer to the string to be searched
+ * @accept: the string with set of characters to be considered for the span
+ * Return: the length of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b, count, checker;
+
+	a = 0;
+	count = 0;
+	while (s[a] != '\0')
+	{
+		b = 0;
+		checker = 0;
+		while (accept[b] != '\0')
+		{
+			if (accept[b] == s[a])
+			{
+				count++;
+				checker = 1;
+				break;
+			}
+			b++;
+		}
+
+		if (checker == 0)
+			return (count);
+		a++;
+	}
+	return (count);
+}
+
+
