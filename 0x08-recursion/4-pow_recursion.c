8000ddb5300dcfa4b75b904ec860b1ae979355c5
cherishson <lovelyjah20@gmail.com>
2023-10-29 09:59:31 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _poe_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: theinteger
  * @y: the power
  * Return: returns the result
