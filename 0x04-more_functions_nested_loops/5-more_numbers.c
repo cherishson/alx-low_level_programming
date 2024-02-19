19ba588a36bee02910d2a4d54624ef3499fd68eb
cherishson <lovelyjah20@gmail.com>
2024-02-19 09:05:21 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times thenumbers, fron 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int j, i = 0;
+
+	while(i < 10)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		i++;
+
+		_putchar('\n');
+	}
+}
