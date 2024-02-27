a97e46a0e7c5eeaf98940ebbfcda5a2f9a9f3659
cherishson <lovelyjah20@gmail.com>
2024-02-26 18:01:02 -0800

    1-argc.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of pointer to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
